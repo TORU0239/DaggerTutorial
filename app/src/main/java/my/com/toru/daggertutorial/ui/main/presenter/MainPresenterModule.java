6fdc7a72a95b80c86c840807c064835f2843f884
Toru Choi <toru.choi@11street.my>
2018-05-02 14:11:24 +0800

    dagger applied to retrofit

--- app/src/main/java/my/com/toru/daggertutorial/ui/main/presenter/MainPresenterModule.java
+++ app/src/main/java/my/com/toru/daggertutorial/ui/main/presenter/MainPresenterModule.java
@@ -3,16 +3,18 @@ package my.com.toru.daggertutorial.ui.main.presenter;
 import dagger.Module;
 import dagger.Provides;
 import my.com.toru.daggertutorial.di.MainActivityModule;
+import my.com.toru.daggertutorial.remote.RemoteModule;
 import my.com.toru.daggertutorial.ui.main.MainActivity;
 import my.com.toru.daggertutorial.ui.main.view.MainView;
+import retrofit2.Retrofit;
 
-@Module(includes = MainActivityModule.class)
+@Module(includes = {MainActivityModule.class, RemoteModule.class})
 public class MainPresenterModule {
 
     public MainPresenterModule() { }
 
     @Provides
-    MainPresenterImp getMainP(final MainActivity activity){
-        return new MainPresenterImp(activity);
+    MainPresenterImp getMainP(final MainActivity activity, Retrofit retrofit){
+        return new MainPresenterImp(activity, retrofit);
     }
 }
