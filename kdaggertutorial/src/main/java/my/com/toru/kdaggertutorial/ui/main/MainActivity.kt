09d96dbcaeec64a5033f42657e0e61ac9843e4ea
Toru Choi <toru.choi@11street.my>
2018-05-12 16:14:53 +0800

    package, and add dagger dependencies

--- kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/main/MainActivity.kt
+++ kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package my.com.toru.kdaggertutorial
+package my.com.toru.kdaggertutorial.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import my.com.toru.kdaggertutorial.R
 
 class MainActivity : AppCompatActivity() {
 
