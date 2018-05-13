0214b1462487e176b442bf8ce9f7b2331b0ff0f7
Toru Choi <toru.choi@11street.my>
2018-05-13 21:22:49 +0800

    dummy detail activity and theme

--- kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/detail/DetailActivity.kt
+++ kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/detail/DetailActivity.kt
@@ -9,5 +9,6 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        supportActionBar?.title = "DetailActivity"
     }
 }
