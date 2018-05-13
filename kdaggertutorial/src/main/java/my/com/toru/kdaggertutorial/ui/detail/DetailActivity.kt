07996dd696e8d86468dd9b76ac397c10fd3ccb34
Toru Choi <toru.choi@11street.my>
2018-05-13 21:13:33 +0800

    clicklistener

---
+++ kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package my.com.toru.kdaggertutorial.ui.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import my.com.toru.kdaggertutorial.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/main/MainActivity.kt
+++ kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/ui/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package my.com.toru.kdaggertutorial.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import my.com.toru.kdaggertutorial.R
 import my.com.toru.kdaggertutorial.app.KDaggerTutorialApp
 import my.com.toru.kdaggertutorial.di.DummyUtil
+import my.com.toru.kdaggertutorial.ui.detail.DetailActivity
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +26,9 @@ class MainActivity : AppCompatActivity() {
                 .inject(this)
 
         Log.w(MainActivity::class.java.simpleName, testclass.testCode())
+
+        main_btn.setOnClickListener{
+            startActivity(Intent(this@MainActivity, DetailActivity::class.java))
+        }
     }
 }
