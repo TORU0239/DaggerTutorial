6e098f00d79cd7c458e6974fb7217df421080437
Toru Choi <toru.choi@11street.my>
2018-05-21 18:35:22 +0800

    import optimized

--- kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/di/DetailFragmentComponent.kt
+++ kdaggertutorial/src/main/java/my/com/toru/kdaggertutorial/di/DetailFragmentComponent.kt
@@ -3,10 +3,7 @@ package my.com.toru.kdaggertutorial.di
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import dagger.Subcomponent
-import my.com.toru.kdaggertutorial.presenter.DetailContracts
 import my.com.toru.kdaggertutorial.ui.detail.DetailFragment
-import my.com.toru.kdaggertutorial.ui.detail.DetailPresenterImp
 
 @Component(modules = [DetailModule::class, DetailModule2::class, DummyUtilModule2::class, DummyUtilModule3::class])
 interface DetailFragmentComponent {
